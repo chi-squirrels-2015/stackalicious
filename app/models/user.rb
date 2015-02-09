5bad1a009deba4dde465be67cbec7203c85e7e49
Kara A Carrell & Matthew Gray <pair+karaAJC.mtthwgry@devbootcamp.com>
2015-02-09 15:12:24 -0600

    add user auth using bcrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,18 @@ class User < ActiveRecord::Base
   has_many :questions, foreign_key: :creator_id
   has_many :answers, foreign_key: :creator_id
   has_many :votes, foreign_key: :voter_id
+
+  def password
+    @password ||= BCrypt::Password.new(password_hash)
+  end
+
+  def password=(password)
+    @password = BCrypt::Password.create(password)
+    self.password_hash = @password
+  end
+
+  def self.authenticate(email, password)
+    user = User.find_by(email: email)
+    user if user && user.password == password
+  end
 end
---
+++ db/seeds.rb
@@ -0,0 +1,3 @@
+User.create(name: "Matthew Gray", email: "m@m.com", password: "password")
+User.create(name: "Kara Carrell", email: "k@k.com", password: "password")
+
